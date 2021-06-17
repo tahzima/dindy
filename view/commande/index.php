ead6f8c75c17f0c45a60c64fec609e3f4ebddfe1
tahzima <tahzima.i@gmail.com>
2021-06-17 14:50:49 +0100

    V-2.0

--- view/commande/index.php
+++ view/commande/index.php
@@ -5,9 +5,9 @@
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Commande</title>
-      <link rel="stylesheet" href="http://localhost/gestionEmplois/view/css/style.css">
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap4.min.css">
+      <link rel="stylesheet" href="http://localhost/dindy/view/css/style.css">
+      <link rel="stylesheet" href="http://localhost/dindy/view/bootstrap/css/bootstrap.css">
+      <link rel="stylesheet" href="http://localhost/dindy/view/bootstrap/css/bootstrap.min.css">
   </head>
   <body>
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
